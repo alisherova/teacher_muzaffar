6e4f096e358063d93b5f53ac2213d9505aa65c2c
alisherova <alisherovanurjahon@gmail.com>
2023-05-01 21:54:21 +0500

    test tailwind working or not

--- layouts/navbar/index.tsx
+++ layouts/navbar/index.tsx
@@ -1,34 +1,32 @@
 import Link from 'next/link';
-import {Menu} from 'antd';
-import {UserOutlined, InfoCircleOutlined} from '@ant-design/icons';
+import { NavbarWrapper } from './style'
 
 const Navbar = () => {
   return (
-    <Menu mode='horizontal'
-          style={{display: 'flex', justifyContent: 'space-between'}}>
-      <Menu.Item key='logo'>
+    <NavbarWrapper className='flex justify-around p-5'>
+      <div key='logo'>
         <Link href='/'>
           <img src='/vercel.svg'
-               alt='Logo'
-               style={{width: '50px'}}/>
+            alt='Logo'
+            style={{ width: '50px' }} />
         </Link>
-      </Menu.Item>
-      <div>
-        <Menu.Item key='home'
-                   style={{marginRight: '0'}}>
-          <Link href='/'>Home</Link>
-        </Menu.Item>
-        <Menu.Item key='courses'
-                   style={{marginRight: '0'}}>
-          <Link href='/courses'>Courses</Link>
-        </Menu.Item>
-        <Menu.Item key='profile'
-                   icon={<UserOutlined/>}
-                   style={{marginRight: '0'}}>
-          <Link href='/profile'>Profile</Link>
-        </Menu.Item>
       </div>
-    </Menu>
+      <div className="flex bg-red-400">
+        <div key='home'
+          style={{ marginRight: '0' }}>
+          <Link href='/' className='text-red-400'>Home</Link>
+        </div>
+        <div key='courses'
+          style={{ marginRight: '0' }}>
+          <Link href='/courses'>Courses</Link>
+        </div>
+        <div key='profile'
+          style={{ marginRight: '0' }}>
+
+          <Link href='/profile'>Profile</Link>
+        </div>
+      </div>
+    </NavbarWrapper>
   );
 };
 
